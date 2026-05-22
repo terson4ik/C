2bc0e2361e1685ef9d60c9ef5af4cf425f2517fa
Vladislav <vladislavsultan846@gmail.com>
2026-05-22 23:14:45 +0300

    some funny things

--- square.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    for (int i = 1; i <= 25; i++)
-        printf("%d * %d\t = %d\n", i, i, i*i);
-    return 0;
-}
---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void lolka(){
+    static int tess = 1;
+    printf("%d\n", tess);
+    tess++;
+}
+
+int main() {
+    int x = 28;
+    int *p;
+    p = &x;
+    lolka();
+    lolka();
+    lolka();
+    lolka();
+    lolka();
+    lolka();
+    lolka();
+    lolka();
+}
